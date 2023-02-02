b44fcaf4f261ad4987bc213767dec403822fb8ba
Daniel Cristian <danielcrisap@gmail.com>
2023-02-02 20:58:50 -0300

    feat: :sparkles: inicial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-slf4j-json-layout
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_slf4j_json_layout/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-slf4j-json-layout.core-test
+  (:require [clojure.test :refer :all]
+            [clj-slf4j-json-layout.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
